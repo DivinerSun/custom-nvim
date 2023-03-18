f21faaf7e52d902ccfaddc5bb08b091d50184ba8
Diviner <sqq_job@sina.com>
2023-03-18 19:39:58 +0800

    feat: 代码高亮颜色和代码补全配置

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -11,4 +11,42 @@ return {
 			require("config.colorscheme")
 		end,
 	},
+	-- 代码颜色
+	{
+		"nvim-treesitter/nvim-treesitter",
+		event = "BufRead",
+		cmd = {
+			"TSBufDisable",
+			"TSBufEnable",
+			"TSBufToggle",
+			"TSDisable",
+			"TSEnable",
+			"TSToggle",
+			"TSInstall",
+			"TSInstallInfo",
+			"TSInstallSync",
+			"TSModuleInfo",
+			"TSUninstall",
+			"TSUpdate",
+			"TSUpdateSync",
+		},
+		build = ":TSUpdate",
+		config = function()
+			require("config.treesitter")
+		end,
+	},
+	-- 代码补全
+	{
+		"hrsh7th/nvim-cmp",
+		event = "BufWinEnter",
+		dependencies = {
+			"hrsh7th/cmp-nvim-lsp",
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-path",
+			"saadparwaiz1/cmp_luasnip",
+		},
+		config = function()
+			require("config.cmp")
+		end,
+	},
 }
