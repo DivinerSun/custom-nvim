b943953c3a51015d869b515d3b08332580e7fcc6
Diviner <sqq_job@sina.com>
2023-10-13 09:35:07 +0800

    feat: 更新indent-blankline.nvim插件配置项

--- lua/config/indent.lua
+++ lua/config/indent.lua
@@ -1,4 +1,5 @@
-local status_ok, indent_blankline = pcall(require, "indent_blankline")
+-- local status_ok, indent_blankline = pcall(require, "indent_blankline")
+local status_ok, ibl = pcall(require, "ibl")
 if not status_ok then
 	return
 end
@@ -57,14 +58,45 @@ vim.wo.colorcolumn = "99999"
 -- vim.opt.listchars:append "space:"
 -- vim.opt.listchars:append "eol:↴"
 
-indent_blankline.setup({
-	-- show_end_of_line = true,
-	-- space_char_blankline = " ",
-	show_current_context = true,
-	-- show_current_context_start = true,
-	-- char_highlight_list = {
-	--   "IndentBlanklineIndent1",
-	--   "IndentBlanklineIndent2",
-	--   "IndentBlanklineIndent3",
-	-- },
+-- indent_blankline.setup({
+-- 	-- show_end_of_line = true,
+-- 	-- space_char_blankline = " ",
+-- 	show_current_context = true,
+-- 	-- show_current_context_start = true,
+-- 	-- char_highlight_list = {
+-- 	--   "IndentBlanklineIndent1",
+-- 	--   "IndentBlanklineIndent2",
+-- 	--   "IndentBlanklineIndent3",
+-- 	-- },
+-- })
+
+-- indent-blankline V3
+local highlight = {
+	"RainbowRed",
+	"RainbowYellow",
+	"RainbowBlue",
+	"RainbowOrange",
+	"RainbowGreen",
+	"RainbowViolet",
+	"RainbowCyan",
+}
+
+local hooks = require("ibl.hooks")
+-- create the highlight groups in the highlight setup hook, so they are reset
+-- every time the colorscheme changes
+hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
+	vim.api.nvim_set_hl(0, "RainbowRed", { fg = "#E06C75" })
+	vim.api.nvim_set_hl(0, "RainbowYellow", { fg = "#E5C07B" })
+	vim.api.nvim_set_hl(0, "RainbowBlue", { fg = "#61AFEF" })
+	vim.api.nvim_set_hl(0, "RainbowOrange", { fg = "#D19A66" })
+	vim.api.nvim_set_hl(0, "RainbowGreen", { fg = "#98C379" })
+	vim.api.nvim_set_hl(0, "RainbowViolet", { fg = "#C678DD" })
+	vim.api.nvim_set_hl(0, "RainbowCyan", { fg = "#56B6C2" })
+end)
+
+vim.g.rainbow_delimiters = { highlight = highlight }
+ibl.setup({
+	indent = { highlight = highlight },
+	scope = { highlight = highlight },
 })
+hooks.register(hooks.type.SCOPE_HIGHLIGHT, hooks.builtin.scope_highlight_from_extmark)
